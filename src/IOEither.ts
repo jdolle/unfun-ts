47dc7503e70f9f8bf5606fd65658a68d96388f76
Jeff Dolle <1841898+jdolle@users.noreply.github.com>
2020-02-16 19:32:56 -0800

    Add husky and lint-staged

--- src/IOEither.ts
+++ src/IOEither.ts
@@ -13,8 +13,6 @@ import { throwError } from './util'
  * @since 0.0.1
  * Converts a IOEither to a IO. If the IOEither is a left, this IO will throw the left value.
  */
-export function toThrowingIO<E, A>(
-  ioe: IOE.IOEither<E, A>,
-): IO.IO<A> {
+export function toThrowingIO<E, A>(ioe: IOE.IOEither<E, A>): IO.IO<A> {
   return pipe(ioe, IOE.fold(throwError, IO.of))
 }
