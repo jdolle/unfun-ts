351864a8ae6dc9663183f91bea7818305dcf7f93
Jeff Dolle <1841898+jdolle@users.noreply.github.com>
2020-02-21 09:41:54 -0800

    Add Either.toTuple function

--- src/Either.ts
+++ src/Either.ts
@@ -19,3 +19,24 @@ import { throwError } from './util'
 export function getOrThrow<E, A>(either: E.Either<E, A>): A {
   return pipe(either, E.fold(throwError, identity))
 }
+
+/**
+ * @since 1.1.0
+ *
+ * Returns a tuple matching the either's state.
+ * If the either is a left, this tuple will be [leftValue, undefined]
+ * If the either is a right, this tuple will be [undefined, rightValue]
+ *
+ * @param either Either to extract value from
+ */
+export function toTuple<E, A>(
+  either: E.Either<E, A>,
+): [E | undefined, A | undefined] {
+  return pipe(
+    either,
+    E.fold<E, A, [E | undefined, A | undefined]>(
+      left => [left, undefined],
+      right => [undefined, right],
+    ),
+  )
+}
